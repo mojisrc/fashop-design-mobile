394e5377e21c7fa1f9c538ef366916ea315ed28a
李杰 <lj706517903@163.com>
2018-06-07 16:44:47 +0800

    Change input-item

---
+++ fashop-components/input-item/CustomKeyboard.tsx
@@ -0,0 +1,158 @@
+import classnames from 'classnames';
+import React from 'react';
+import TouchFeedback from 'rmc-feedback';
+import { Omit } from '../../ant-design-mobile/components/_util/types';
+
+export type HTMLTableDataProps = Omit<
+  React.HTMLProps<HTMLTableDataCellElement>,
+  'onClick'
+>;
+
+export interface KeyboardItemProps extends HTMLTableDataProps {
+  prefixCls?: string;
+  tdRef?: React.Ref<HTMLTableDataCellElement>;
+  onClick: (
+    event: React.MouseEvent<HTMLTableDataCellElement>,
+    value: string,
+  ) => void;
+}
+export class KeyboardItem extends React.Component<KeyboardItemProps, any> {
+  static defaultProps = {
+    prefixCls: 'am-number-keyboard',
+    onClick: () => {},
+    disabled: false,
+  };
+
+  render() {
+    const {
+      prefixCls,
+      onClick,
+      className,
+      disabled,
+      children,
+      tdRef,
+      ...restProps,
+    } = this.props;
+    let value = children;
+    if (className === 'keyboard-delete') {
+      value = 'delete';
+    } else if (className === 'keyboard-hide') {
+      value = 'hide';
+    } else if (className === 'keyboard-confirm') {
+      value = 'confirm';
+    }
+
+    const wrapCls = classnames(`${prefixCls}-item`, className);
+    return (
+      <TouchFeedback activeClassName={`${prefixCls}-item-active`}>
+        <td
+          ref={tdRef}
+          // tslint:disable-next-line:jsx-no-multiline-js
+          onClick={e => {
+            onClick(e, value as string);
+          }}
+          className={wrapCls}
+          {...restProps}
+        >
+          {children}
+        </td>
+      </TouchFeedback>
+    );
+  }
+}
+
+class CustomKeyboard extends React.Component<any, any> {
+  static defaultProps = {
+    prefixCls: 'am-number-keyboard',
+  };
+
+  linkedInput: any;
+  antmKeyboard: HTMLDivElement | null;
+  confirmDisabled: boolean;
+  confirmKeyboardItem: HTMLTableDataCellElement | null;
+
+  onKeyboardClick = (
+    e: React.MouseEvent<HTMLTableDataCellElement>,
+    value: string,
+  ) => {
+    e.nativeEvent.stopImmediatePropagation();
+    if (value === 'confirm' && this.confirmDisabled) {
+      return null;
+    } else {
+      if (this.linkedInput) {
+        this.linkedInput.onKeyboardClick(value);
+      }
+    }
+  }
+
+  renderKeyboardItem = (item: string, index: number) => {
+    return (
+      <KeyboardItem
+        onClick={this.onKeyboardClick}
+        key={`item-${item}-${index}`}
+      >
+        {item}
+      </KeyboardItem>
+    );
+  }
+  render() {
+    const { prefixCls, confirmLabel } = this.props;
+
+    const wrapperCls = classnames(
+      `${prefixCls}-wrapper`,
+      `${prefixCls}-wrapper-hide`,
+    );
+
+    return (
+      <div className={wrapperCls} ref={el => (this.antmKeyboard = el)}>
+        <table>
+          <tbody>
+            <tr>
+              {['1', '2', '3'].map((item, index) =>
+              // tslint:disable-next-line:jsx-no-multiline-js
+                this.renderKeyboardItem(item, index),
+              )}
+              <KeyboardItem
+                className="keyboard-delete"
+                rowSpan={2}
+                onClick={this.onKeyboardClick}
+              />
+            </tr>
+            <tr>
+              {['4', '5', '6'].map((item, index) =>
+              // tslint:disable-next-line:jsx-no-multiline-js
+                this.renderKeyboardItem(item, index),
+              )}
+            </tr>
+            <tr>
+              {['7', '8', '9'].map((item, index) =>
+              // tslint:disable-next-line:jsx-no-multiline-js
+                this.renderKeyboardItem(item, index),
+              )}
+              <KeyboardItem
+                className="keyboard-confirm"
+                rowSpan={2}
+                onClick={this.onKeyboardClick}
+                tdRef={el => (this.confirmKeyboardItem = el)}
+              >
+                {confirmLabel}
+              </KeyboardItem>
+            </tr>
+            <tr>
+              {['.', '0'].map((item, index) =>
+              // tslint:disable-next-line:jsx-no-multiline-js
+                this.renderKeyboardItem(item, index),
+              )}
+              <KeyboardItem
+                className="keyboard-hide"
+                onClick={this.onKeyboardClick}
+              />
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default CustomKeyboard;
---
+++ fashop-components/input-item/demo/money.md
@@ -0,0 +1,85 @@
+---
+order: 0
+title:
+  zh-CN: 金额键盘
+  en-US: Money input
+---
+
+## zh-CN
+
+受控组件建议使用([rc-form ](https://github.com/react-component/form))
+
+## en-US
+
+Recommended use of [rc-form ](https://github.com/react-component/form) for controlled component.
+
+````jsx
+import { List, InputItem } from 'fashopd-mobile';
+import { createForm } from 'rc-form';
+
+class H5NumberInputExample extends React.Component {
+  state = {
+    type: 'money',
+  }
+  render() {
+    const { getFieldProps } = this.props.form;
+    const { type } = this.state;
+    return (
+      <div>
+        <List>
+          <InputItem
+            {...getFieldProps('money3')}
+            type={type}
+            defaultValue={100}
+            placeholder="start from left"
+            clear
+            moneyKeyboardAlign="left"
+          >光标在左</InputItem>
+          <InputItem
+            type={type}
+            placeholder="start from right"
+            clear
+            onChange={(v) => { console.log('onChange', v); }}
+            onBlur={(v) => { console.log('onBlur', v); }}
+          >光标在右</InputItem>
+          <InputItem
+            {...getFieldProps('money2', {
+              normalize: (v, prev) => {
+                if (v && !/^(([1-9]\d*)|0)(\.\d{0,2}?)?$/.test(v)) {
+                  if (v === '.') {
+                    return '0.';
+                  }
+                  return prev;
+                }
+                return v;
+              },
+            })}
+            type={type}
+            placeholder="money format"
+            ref={el => this.customFocusInst = el}
+            clear
+          >数字键盘</InputItem>
+          <List.Item>
+            <div
+              style={{ width: '100%', color: '#108ee9', textAlign: 'center' }}
+              onClick={() => this.customFocusInst.focus()}
+            >
+              click to focus
+            </div>
+          </List.Item>
+        </List>
+      </div>
+    );
+  }
+}
+
+const H5NumberInputExampleWrapper = createForm()(H5NumberInputExample);
+ReactDOM.render(<H5NumberInputExampleWrapper />, mountNode);
+````
+
+````css
+.demoTitle:before,
+.demoTitle:after {
+  border-bottom: none;
+}
+````
